205c0d7b8febc757d51b8e51053b7d7e3ea83b91
seif565 <assassin56731@gmail.com>
2024-08-22 23:29:40 +0300

    Fix issue with ticket repository and rename ticket color.

---
+++ TicketingSystem.Infrastructure/Migrations/20240822202809_RenameTicketStatusToColor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketingSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTicketStatusToColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TicketStatus",
+                table: "Tickets",
+                newName: "TicketColor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TicketColor",
+                table: "Tickets",
+                newName: "TicketStatus");
+        }
+    }
+}
